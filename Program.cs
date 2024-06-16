e4cbbae7f673e0061f19ca43b9005f64cf1eec35
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-06-16 16:10:52 -0400

    Added input logic Added logic to loop through cards and output to console

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+            string[] cards = [];
 
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid number!");
+            }
         }
     }
 }
