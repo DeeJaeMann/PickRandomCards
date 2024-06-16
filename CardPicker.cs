ff92729bd9d8aa6cc0bc75ff15e152829c387859
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-06-16 15:54:29 -0400

    defined random logic for card picking in RandomSuit and RandomValue

--- CardPicker.cs
+++ CardPicker.cs
@@ -8,6 +8,7 @@ namespace PickRandomCards
 {
     internal class CardPicker
     {
+        static Random random = new Random();
         public static string[] PickSomeCards(int numberOfCards)
         {
             string[] pickedCards = new string[numberOfCards];
@@ -20,12 +21,36 @@ namespace PickRandomCards
 
         private static string RandomSuit()
         {
-            throw new NotImplementedException();
+            // get a random number from 1 to 4
+            int value = random.Next(1, 5);
+            /* assign suits based on number:
+             * 1 - Spades
+             * 2 - Hearts
+             * 3 - Clubs
+             * 4 - Diamonds
+             */
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+            return "Diamonds";
         }
 
         private static string RandomValue()
         {
-            throw new NotImplementedException();
+            // get a random number from 1 to 13
+            int value = random.Next(1, 14);
+            /* If value matches numbers associated with face cards, return the face card value
+             * 1 - Ace
+             * 11 - Jack
+             * 12 - Queen
+             * 13 - King
+             * Any other value is converted to string and returned
+             */
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
         }
     }
 }
